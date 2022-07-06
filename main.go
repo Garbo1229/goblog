a4e570069e4b669a73832e44d433c16d86258ed1
GarboFeng <i@garbo.me>
2022-07-06 17:02:56 +0800

    创建数据表

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"html/template"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
-	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 var router = mux.NewRouter()
+var db *sql.DB
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
@@ -126,7 +131,51 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 }
+
+func initDB(){
+	var err error
+	config := mysql.Config{
+		User:"root",
+		Passwd: "root",
+		Addr:"127.0.0.1:3306",
+		Net: "tcp",
+		DBName: "goblog",
+		AllowNativePasswords: true,
+	}
+	db,err =sql.Open("mysql",config.FormatDSN())
+
+	checkError(err)
+
+	// 设置最大连接数
+	db.SetMaxOpenConns(25)
+	// 设置最大空闲连接数
+	db.SetMaxIdleConns(25)
+	// 设置每个链接的过期时间
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	// 尝试连接，失败会报错
+	err = db.Ping()
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func createTables() {
+	createArticlesSQL := `CREATE TABLE IF NOT EXISTS articles(
+    id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    body longtext COLLATE utf8mb4_unicode_ci
+); `
+
+	_, err := db.Exec(createArticlesSQL)
+	checkError(err)
+}
 func main() {
+	initDB()
+	createTables()
 	router.StrictSlash(true)
 
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")

591f21bc50715e791d8f4e45b916ca5bc94c8019
GarboFeng <i@garbo.me>
2022-07-06 10:46:21 +0800

    Init

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+func handleFunc(w http.ResponseWriter,r *http.Request){
+	fmt.Fprint(w,"<h1>golang</h1>")
+}
+func main(){
+	http.HandleFunc("/",handleFunc)
+	http.ListenAndServe(":3000",nil)
+}

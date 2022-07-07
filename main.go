14bbcb807375e9ae80dc49b62c40bf94bf52e32e
GarboFeng <i@garbo.me>
2022-07-07 10:33:22 +0800

    重构 RouteName2URL

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"goblog/pkg/route"
 	"html/template"
 	"log"
 	"net/http"
@@ -16,7 +17,7 @@ import (
 	"unicode/utf8"
 )
 
-var router = mux.NewRouter()
+var router *mux.Router
 var db *sql.DB
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -71,7 +72,7 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 		// 4. 读取成功，显示文章
 		tmpl, err := template.New("show.gohtml").
 			Funcs(template.FuncMap{
-				"RouteName2URL": RouteName2URL,
+				"RouteName2URL": route.Name2URL,
 				"Int64ToString": Int64ToString,
 			}).
 			ParseFiles("resources/views/articles/show.gohtml")
@@ -449,17 +450,6 @@ func articlesDeleteHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// RouteName2URL 通过路由名称来获取 URL
-func RouteName2URL(routeName string, pairs ...string) string {
-	url, err := router.Get(routeName).URL(pairs...)
-	if err != nil {
-		checkError(err)
-		return ""
-	}
-
-	return url.String()
-}
-
 // Int64ToString 将 int64 转换为 string
 func Int64ToString(num int64) string {
 	return strconv.FormatInt(num, 10)
@@ -481,6 +471,10 @@ func (a Article) Delete() (rowsAffected int64, err error) {
 func main() {
 	initDB()
 	//createTables()
+
+	route.Initialize()
+	router = route.Router
+
 	router.StrictSlash(true)
 
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")

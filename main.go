6629f4cb57e2500a050f1019c9a9477d0ffdf575
GarboFeng <i@garbo.me>
2022-07-12 10:35:24 +0800

    配置和环境变量

--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"goblog/app/middlewares"
 	"goblog/bootstrap"
-	"goblog/pkg/logger"
+	"goblog/config"
+	c "goblog/pkg/config"
 	"net/http"
 )
+
+func init() {
+	// 初始化配置信息
+	config.Initialize()
+}
 
 func main() {
 	bootstrap.SetupDB()
 	router := bootstrap.SetupRoute()
 
-	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
-	logger.LogError(err)
+	http.ListenAndServe(":"+c.GetString("app.port"), middlewares.RemoveTrailingSlash(router))
 }

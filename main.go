ce27e0de9ae5824ba4d649dcbe9ea7f8f4e795f3
GarboFeng <i@garbo.me>
2022-07-13 11:54:43 +0800

    embed 打包模板文件

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"embed"
 	_ "github.com/go-sql-driver/mysql"
 	"goblog/app/middlewares"
 	"goblog/bootstrap"
 	"goblog/config"
 	c "goblog/pkg/config"
+	"goblog/pkg/logger"
 	"net/http"
 )
+
+//go:embed resources/views/articles/*
+//go:embed resources/views/auth/*
+//go:embed resources/views/categories/*
+//go:embed resources/views/layouts/*
+var tplFS embed.FS
 
 func init() {
 	// 初始化配置信息
@@ -15,8 +23,14 @@ func init() {
 }
 
 func main() {
+	// 初始化 SQL
 	bootstrap.SetupDB()
-	router := bootstrap.SetupRoute()
 
-	http.ListenAndServe(":"+c.GetString("app.port"), middlewares.RemoveTrailingSlash(router))
+	// 初始化模板
+	bootstrap.SetupTemplate(tplFS)
+	// 初始化路由绑定
+	router := bootstrap.SetupRoute()
+	
+	err := http.ListenAndServe(":"+c.GetString("app.port"), middlewares.RemoveTrailingSlash(router))
+	logger.LogError(err)
 }
